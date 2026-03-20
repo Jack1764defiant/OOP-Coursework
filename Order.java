7f973aff530df3615ecfedc4310ddf9d286d7aa4
Jack_S <js127127@outlook.com>
2026-03-20 09:36:53 +0100

    Order and StockItem classes, OrderStatus enum and StockException filled in

--- Order.java
+++ Order.java
@@ -1,3 +1,53 @@
-public class Order {
+import java.util.ArrayList;
+import java.time.LocalDateTime;
 
+public class Order {
+    //If this boolean is false, this is an order the company has placed from a supplier
+    //If this boolean is true, this is an order another company has placed with us
+    private final boolean sale;
+    //The ID of the supplier or client depending on whether this is a sale or a purchase
+    private final int otherPartiesID;
+    private OrderStatus orderStatus;
+    private final ArrayList<StockItem> itemsOrdered;
+    private final LocalDateTime orderTime; 
+
+    public Order(int _otherPartiesID, ArrayList<StockItem> _itemsOrdered, LocalDateTime _orderTime, boolean _sale){
+        otherPartiesID = _otherPartiesID;
+        itemsOrdered = _itemsOrdered;
+        orderTime = _orderTime;
+        sale = _sale;
+        orderStatus = OrderStatus.Placed;
+    }
+
+    public float GetOrderCost(){
+        float total = 0;
+        for (StockItem itemOrder : itemsOrdered){
+            total += itemOrder.GetTotalCost();
+        }
+        return total;
+    }
+
+    public int GetID(){
+        return otherPartiesID;
+    }
+
+    public boolean isSale(){
+        return sale;
+    }
+
+    public OrderStatus GetOrderStatus(){
+        return orderStatus;
+    }
+
+    public void SetOrderStatus(OrderStatus newOrderStatus){
+        orderStatus = newOrderStatus;
+    }
+
+    public LocalDateTime GetOrderTime(){
+        return orderTime;
+    }
+
+    public ArrayList<StockItem> GetItemsOrdered(){
+        return itemsOrdered;
+    }
 }
--- StockItem.java
+++ StockItem.java
@@ -1,5 +1,38 @@
 public class StockItem{
+    private final String itemName;
+    private int amount;
+    private final float individualCost;
 
+    public StockItem(String _itemName, int _amount, float _individualCost){
+        itemName = _itemName;
+        amount = _amount;
+        individualCost = _individualCost;
+    }
+
+    public String GetItemName(){
+        return itemName;
+    }
+
+    public int GetAmount(){
+        return amount;
+    }
+
+    public void SetAmount(int newAmount) throws StockException{
+        if (newAmount >= 0){
+            amount = newAmount;
+        }
+        else{
+            throw new StockException("Invalid amount of stock - less than 0 (" + String.valueOf(newAmount) + ")");
+        }
+    }
+
+    public float GetIndividualCost(){
+        return individualCost;
+    }
+
+    public float GetTotalCost(){
+        return individualCost * amount;
+    }
 }
 
 
